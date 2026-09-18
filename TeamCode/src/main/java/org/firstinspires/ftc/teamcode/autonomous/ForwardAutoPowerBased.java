60c1e8047149d00f4092c7c9ef0ff04e05538b9f
Diego Gonzalez <dlgonzalez1109@gmail.com>
2026-09-18 17:21:52 -0500

    Update ForwardAutoPowerBased.java
    - (QoL) Now extends AutoBaseFile instead of LinearOpMode
    - (Add) Now has Encoder Launch Functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ForwardAutoPowerBased.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ForwardAutoPowerBased.java
@@ -1,17 +1,39 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.bylazar.telemetry.PanelsTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 @Autonomous(name="Auto_Program_Blue", group = "Auto")
-public class ForwardAutoPowerBased extends LinearOpMode {
+public class ForwardAutoPowerBased extends  AutoBaseFile {
     public DcMotor frontLeft = null;
     public DcMotor frontRight = null;
     public DcMotor backLeft = null;
     public DcMotor backRight = null;
 
+    public enum LAUNCH_POSITION {
+
+        //If we are adding aimbot, cords can go in here too.
+
+        BLUE_MID(10,18.4, 1000, 8),
+        BLUE_PILLAR_FAR(10, 17.7, 1200, 6),
+        BLUE_WALL_FAR(10, 15, 1500, 5), //untested values
+
+        RED_MID(10, 14.3, 1000, 8),
+        RED_PILLAR_FAR(11, 14.3, 1200, 6),
+        RED_WALL_FAR(10, 15, 1500, 5); // untested values
+
+        final double P, F, velocityNeeded, durationSeconds;
+
+        LAUNCH_POSITION(double P, double F, double velocityNeeded, double durationSeconds) {
+            this.P = P;
+            this.F = F;
+            this.velocityNeeded = velocityNeeded;
+            this.durationSeconds = durationSeconds;
+        }
+    }
     public void runOpMode(){
         this.backLeft = (DcMotor) hardwareMap.get(DcMotor.class, "BackLeftmotor");
         this.backRight = (DcMotor) hardwareMap.get(DcMotor.class, "BackRightmotor");
@@ -23,13 +45,7 @@ public class ForwardAutoPowerBased extends LinearOpMode {
 
         waitForStart();
 
-        forward(1450);
-        strafeLeft(3700);
-        rotateRight(0);
-        strafeLeft(700);
-        //grab
 
-        forward(3500);
     }
 
     public void forward(int time){
@@ -87,7 +103,7 @@ public class ForwardAutoPowerBased extends LinearOpMode {
         frontRight.setPower(0);
     }
 
-    public void rotateLeft(int time){
+    public void rotateLeft(){
         backLeft.setPower(-0.5);
         backRight.setPower(0.5);
         frontLeft.setPower(-0.5);
@@ -101,7 +117,7 @@ public class ForwardAutoPowerBased extends LinearOpMode {
         frontRight.setPower(0);
     }
 
-    public void rotateRight(int time){
+    public void rotateRight(){
         backLeft.setPower(0.5);
         backRight.setPower(-0.5);
         frontLeft.setPower(0.5);
@@ -165,6 +181,60 @@ public class ForwardAutoPowerBased extends LinearOpMode {
         backLeft.setPower(0);
         backRight.setPower(0);
 
+
+    }
+
+    public void launch(AutoBaseFile.LAUNCH_POSITION position){
+
+
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(position.P, 0, 0, position.F);
+        outtake.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        outtake.setVelocity(position.velocityNeeded);
+
+        sleep(5000);
+        toggleIntake();
+        toggleMidtake();
+        toggleMidtakeTwo();
+        sleep(4000);
+        toggleIntake();
+        toggleMidtake();
+        toggleMidtakeTwo();
+        outtake.setVelocity(0);
+
+
+
+
+        runTime.reset();
+
+        while (runTime.seconds() <= position.durationSeconds){
+            telemetry.addData("Target Velocity", 1500);
+            telemetry.addData("Current Velocity", outtake.getVelocity());
+            telemetry.addData("Error", 1500 - outtake.getVelocity());
+            telemetry.addLine();
+
+            telemetry.addData("Timer", runTime.milliseconds());
+            telemetry.addData("Timeout", position.durationSeconds);
+
+
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Target Velocity", 1500);
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Current Velocity", outtake.getVelocity());
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Error", 1500 - outtake.getVelocity());
+
+            telemetry.update();
+            PanelsTelemetry.INSTANCE.getTelemetry().update(telemetry);
+
+
+        }
+
+
+        toggleIntake();
+        toggleMidtake();
+
+
+
+
+
+        outtake.setVelocity(0);
 
     }
 
